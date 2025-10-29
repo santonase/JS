ff8c80aa4ad29552e293c96369a1b92f9fa891ca
Sviatoslav <santonase@gmail.com>
2025-10-29 16:40:52 +0200

    'data_types'

---
+++ 3. data types/script.js
@@ -0,0 +1,13 @@
+alert("1111");
+alert(typeof 0);
+alert(typeof "asdasd");
+alert(typeof x);
+
+
+let name = "Ілля";
+
+alert(`привіт ${1}`); // ? привіт 1
+
+alert(`привіт ${"name"}`); // привіт name
+
+alert(`привіт ${name}`); // привіт Ілля
