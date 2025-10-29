f97012b150d94733a165edba170da5ff8286f713
Sviatoslav <santonase@gmail.com>
2025-10-29 15:49:33 +0200

    'variables'

---
+++ 2. values/script.js
@@ -0,0 +1,13 @@
+// 1.
+let name = "Ivan";
+let admin = name;
+alert(admin);
+
+// 2. variables name
+let ourPlanetName = "Earth";
+let currentUserName = "Sviatoslav";
+
+// 3. big letters
+const BIRTHDAY = "18.04.1982";
+const age = BIRTHDAY;
+alert(age);
